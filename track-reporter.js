0869f191c4a719001bed5c3296708f647afb9361
MIYASHITA, Akihiro <akhiro.miyashita@gmail.com>
2022-02-10 15:04:54 +0900

    Use EVENT_TEST_END to increment counter

--- track-reporter.js
+++ track-reporter.js
@@ -5,7 +5,7 @@ const {
   EVENT_RUN_END,
   EVENT_TEST_FAIL,
   EVENT_TEST_PASS,
-  EVENT_SUITE_END,
+  EVENT_TEST_END,
 } = Mocha.Runner.constants;
 
 class TrackReporter {
@@ -14,13 +14,16 @@ class TrackReporter {
     let n = 1;
 
     runner
+      .on(EVENT_TEST_END, () => {
+        ++n;
+      })
       .on(EVENT_TEST_PASS, (test) => {
         // Test#fullTitle() returns the suite name(s)
         // prepended to the test title
-        console.log(`ok ${n++} ${test.fullTitle().trim()}`);
+        console.log(`ok ${n} ${test.fullTitle().trim()}`);
       })
       .on(EVENT_TEST_FAIL, (test, err) => {
-        console.log(`not ok ${n++} ${test.fullTitle().trim()}`);
+        console.log(`not ok ${n} ${test.fullTitle().trim()}`);
         err.message.split("\n").forEach(line => console.log("  " + line));
       })
       .once(EVENT_RUN_END, () => {
